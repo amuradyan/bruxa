2dbf9bdaf0fc4e2ff215932d49a1a42a9b3f0081
Andranik Muradyan <muradianator@gmail.com>
2018-06-24 23:14:45 +0400

    Initial commit

---
+++ src/main/scala/Bruxa.scala
@@ -0,0 +1,12 @@
+/**
+  * Created by spectrum on Jun, 2018
+  */
+import org.apache.spark._
+import org.apache.spark.graphx._
+import org.apache.spark.rdd.RDD
+
+object Bruxa {
+  def main(args: Array[String]): Unit = {
+    println("Yo")
+  }
+}
